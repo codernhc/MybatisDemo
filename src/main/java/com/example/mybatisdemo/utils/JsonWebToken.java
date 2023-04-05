c2668b6be465c02d7b133e95622d2ed73e6b9022
codernhc <a1612180169@outlook.com>
2023-04-05 13:59:23 +0800

    fect(JWT): Authorization: Authorize jwt token authentication

--- src/main/java/com/example/mybatisdemo/utils/JsonWebToken.java
+++ src/main/java/com/example/mybatisdemo/utils/JsonWebToken.java
@@ -1,7 +1,32 @@
 package com.example.mybatisdemo.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 public class JsonWebToken {
-  public static String generateToken(){
-    return "";
+  private static String secret = "coder_sdalsda";
+  private static int expire = 604800;
+
+
+  public static String generateToken(String username) {
+    Date now = new Date();
+    Date expiration = new Date(now.getTime() + 1000 * expire);
+    return Jwts.builder()
+      .setHeaderParam("type", "JWT")
+      .setSubject(username)
+      .setIssuedAt(now)
+      .setExpiration(expiration)
+      .signWith(SignatureAlgorithm.HS512, secret)
+      .compact();
+  }
+
+  public static Claims getClaimsByToken(String token) {
+    return Jwts.parser()
+      .setSigningKey(secret)
+      .parseClaimsJws(token)
+      .getBody();
   }
 }
